388688b3d293aef037f130ae005cfa6d26c661e2
Elijah Chia <chia.elijah@gmail.com>
2023-02-22 01:16:04 +0800

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = { REDACTED };
-const supabaseKey = { REDACTED };
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
